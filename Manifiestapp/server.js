5f9c910d1ba624acd248507e25c5ac0bcef80d0e
Florian Gérard <kingdomflo16@gmail.com>
2021-10-14 21:59:25 +0200

    update build

--- Manifiestapp/server.js
+++ Manifiestapp/server.js
@@ -1,11 +1,23 @@
-const express = require('express');
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(express.static('./www/'));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'www/' }),
-);
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
