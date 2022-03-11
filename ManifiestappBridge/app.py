d2d3d7cbfb4d801cd8486ae8625e06b81adf25e0
FreeCaribou <florian@gerardweb.eu>
2022-03-11 14:27:41 +0100

    Bridge basic route

--- ManifiestappBridge/app.py
+++ ManifiestappBridge/app.py
@@ -1,21 +1,68 @@
-from flask import Flask, jsonify
-import pandas as pd
+from flask import Flask, jsonify, request
+import requests as http_request
+import json
+import utils.get_excel_volunteer as get_excel_volunteer
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config.from_pyfile('settings.py')
 
-@app.route("/")
-def home():
-    return {'name': 'hello world'}
 
-@app.route("/excel")
-def hello_world():
-    file = pd.read_excel(app.config.get("EXCEL_URL"))
+@app.route('/')
+def home():
+    return {'hello': 'world'}
+
+
+# TODO get detail
+# TODO check error
+@app.route('/collaborators/<int:id>/enrolments')
+def collaborator_enrolments(id):
+    url = '{}admin/collaborators/{}/enrolments'.format(
+        app.config.get('BEEPLE_URL'), id)
+    headers = {'Token': request.headers.get('Token')}
+    response = http_request.get(url, headers=headers)
+
+    jResponseEnrolments = json.loads(response.text)['enrolments']
     jsonArray = []
 
-    for r in file.iloc:
+    for e in jResponseEnrolments:
+        detailUrl = '{}admin/collaborators/enrolments/{}'.format(
+            app.config.get('BEEPLE_URL'), e['id'])
+        detailResponse = http_request.get(detailUrl, headers=headers)
+        jResponseDetail = json.loads(detailResponse.text)
         jsonArray.append({
-            "id": int(r['Code Beeple']),
-            "email": r['e-mail'],
+            'id': e['id'],
+            'team': jResponseDetail['team'],
         })
+
     return jsonify(jsonArray)
+
+
+# TODO verify good body to send
+# TODO check error
+# TODO check presence in beeple
+# TODO check presence in our data excel
+@app.route('/auth', methods=['POST'])
+def authentification():
+    body = json.loads(request.data)
+    url = '{}authenticate'.format(app.config.get('BEEPLE_URL'))
+    response = http_request.post(url, json=body)
+
+    jResponse = json.loads(response.text)
+    volunteers_data = get_excel_volunteer.get()
+    volunteer = None
+
+    for e in volunteers_data:
+        if e['email'] == jResponse['email']:
+            volunteer = e
+            break
+
+    if volunteer is not None:
+        return {
+            'id': volunteer['id'],
+            'email': volunteer['email'],
+            'token': jResponse['token']
+        }
+
+    return jResponse
--- ManifiestappBridge/settings.py
+++ ManifiestappBridge/settings.py
@@ -1,3 +1,4 @@
 from os import environ 
 
 EXCEL_URL = environ.get('EXCEL_URL')
+BEEPLE_URL = environ.get('BEEPLE_URL')
