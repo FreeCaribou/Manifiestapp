3cfe82d7fd38015a8b9080f532db1f002cd83783
FreeCaribou <florian@gerardweb.eu>
2023-08-22 18:50:41 +0200

    remove engine node in packagejson

--- Manifiestapp/src/app/shared/services/data/programme/programme.mock.service.ts
+++ Manifiestapp/src/app/shared/services/data/programme/programme.mock.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { cloneDeep } from 'lodash';
 import { Observable, of } from 'rxjs';
 import { EventInterface } from 'src/app/shared/models/Event.interface';
 import { EventDayEnum } from 'src/app/shared/models/EventDay.enum';
@@ -12,19 +11,19 @@ import { IProgrammeService } from './programme.service.interface';
 export class ProgrammeMockService implements IProgrammeService {
 
   getAllProgramme(): Observable<EventInterface[]> {
-    return of(cloneDeep(MOCK_GET_ALL_PROGRAMME));
+    return of(MOCK_GET_ALL_PROGRAMME);
   }
 
   getAllProgrammeFilter(day: string[], venuesId?: string[], organizersId?: string[], eventCategoriesId?: string[]): Observable<EventInterface[]> {
-    return of(cloneDeep(MOCK_GET_ALL_PROGRAMME));
+    return of(MOCK_GET_ALL_PROGRAMME);
   }
 
   getFavoriteProgramme(ids?: string[]): Observable<EventInterface[]> {
-    return of(cloneDeep(MOCK_GET_ALL_PROGRAMME));
+    return of(MOCK_GET_ALL_PROGRAMME);
   }
 
   getEvent(id: string): Observable<EventInterface> {
-    return of(cloneDeep(MOCK_GET_ALL_PROGRAMME.find(x => x.id === id)));
+    return of(MOCK_GET_ALL_PROGRAMME.find(x => x.id === id));
   }
 
 }
