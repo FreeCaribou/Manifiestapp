ac08817aad1dcbc923a1c7b888acbd3e5912a3ab
FreeCaribou <florian@gerardweb.eu>
2023-09-25 19:29:52 +0200

    real data to finish an order

--- manifiestapp-finish-payment/app/finish-payment/page.tsx
+++ manifiestapp-finish-payment/app/finish-payment/page.tsx
@@ -1,13 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 import https from 'https';
 
-// TODO with real data for the ticketing finish order
+// TODO better ui
+// TODO link to mail if problem
+// TODO link to the app or website
 async function askTicket(vivaWalletTransactionId: string) {
-    const res = await axios.get(`${process.env.BASE_URL}api/v2/pages?format=json&type=event.EventPage`,     {
+    const res = await axios.get(`${process.env.BASE_URL}tickets/finishOrderPending/${vivaWalletTransactionId}`, {
         httpsAgent: new https.Agent({
-          rejectUnauthorized: false
+            rejectUnauthorized: false
         })
-      });
+    }).catch(err => {
+        return err?.response?.data;
+    });
     return res;
 }
 
@@ -17,12 +21,20 @@ export default async function FinishPayment({
     searchParams?: { [key: string]: string };
 }) {
     const vivaWalletTransactionId = searchParams?.t;
-    const ticketData = askTicket(vivaWalletTransactionId as string);
+    const ticketData = vivaWalletTransactionId ? askTicket(vivaWalletTransactionId as string) : { data: {} };
     const ticket = await Promise.resolve(ticketData);
+    const error: boolean = !ticket?.data;
 
+    if (error) {
+        return (
+            <div>
+                There is an error - {ticket.message[0]} - ERR CODE {ticket.code}
+            </div>
+        )
+    }
     return (
-        <main>
-            Finish Payment {vivaWalletTransactionId} {ticket.data.meta.total_count}
-        </main>
+        <div>
+            Congratulations, the tickets is selling - REF {ticket?.data?.order?.reference}
+        </div>
     )
 }
