3f209bc6a76080a78b8a2031ce66c546850e433f
FreeCaribou <florian@gerardweb.eu>
2023-11-02 15:51:48 +0100

    More detail when payment is finish

--- manifiestapp-finish-payment/app/finish-payment/page.tsx
+++ manifiestapp-finish-payment/app/finish-payment/page.tsx
@@ -4,7 +4,6 @@ import RedirectToApp from '../shared/redirect-to-app';
 
 // TODO better ui
 // TODO link to mail if problem
-// TODO link to the app or website
 async function askTicket(vivaWalletTransactionId: string) {
     const res = await axios.get(`${process.env.BASE_URL}tickets/finishOrderPending/${vivaWalletTransactionId}`, {
         httpsAgent: new https.Agent({
@@ -28,7 +27,7 @@ export default async function FinishPayment({
 
     const text = error ?
         `There is an error - ${ticket?.message ? ticket?.message[0] : 'unkown'} - ERR CODE ${ticket?.code}`
-        : `Congratulations, the tickets is selling - REF ${ticket?.data?.order?.reference}`;
+        : `Congratulations, the tickets is selling - REF ${ticket?.data?.order?.reference} - You have sold ${ticket?.data.totalTicketsForThisEditionForThisSeller} tickets`;
     return (
         <div>
             <h1 className={error ? 'text-error' : ''}>{text}</h1>
