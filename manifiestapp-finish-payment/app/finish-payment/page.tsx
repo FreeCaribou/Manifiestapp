b7971c399bb70856934a92a27809880e92d29160
FreeCaribou <kingdomflo16@gmail.com>
2024-02-02 10:27:10 +0100

    show data for double ask confirm order

--- manifiestapp-finish-payment/app/finish-payment/page.tsx
+++ manifiestapp-finish-payment/app/finish-payment/page.tsx
@@ -30,13 +30,19 @@ export default async function FinishPayment({
     const messages = {
         en: error ?
             `There is an error - ${ticket?.message ? ticket?.message[0] : 'unkown'} - ERR CODE ${ticket?.code}`
-            : `Congratulations, the tickets is selling - REF ${ticket?.data?.order?.reference} - You have sold ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`,
+            : !ticket?.data?.transactionInProgress ?
+                `Congratulations, the tickets is selling - REF ${ticket?.data?.order?.reference} - You have sold ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`
+                : `${ticket.data.eventsquareReference ? `Order ${ticket.data.eventsquareReference} has been completed` : 'Order in progress, check with the seller'}`,
         fr: error ?
             `Une erreur est survenu - ${ticket?.message ? ticket?.message[0] : 'unkown'} - ERR CODE ${ticket?.code}`
-            : `Félicitation, la commande est finalisée - REF ${ticket?.data?.order?.reference} - Tu as vendu ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`,
+            : !ticket?.data?.transactionInProgress ?
+                `Félicitation, la commande est finalisée - REF ${ticket?.data?.order?.reference} - Tu as vendu ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`
+                : `${ticket.data.eventsquareReference ? `La commande ${ticket.data.eventsquareReference} est terminée` : 'Commande en cours, voir avec le vendeur'}`,
         nl: error ?
             `Er is een fout opgetreden - ${ticket?.message ? ticket?.message[0] : 'unkown'} - ERR CODE ${ticket?.code}`
-            : `Gefeliciteerd, uw bestelling is nu voltooid - REF ${ticket?.data?.order?.reference} - Je hebt verkocht ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`,
+            : !ticket?.data?.transactionInProgress ?
+                `Gefeliciteerd, uw bestelling is nu voltooid - REF ${ticket?.data?.order?.reference} - Je hebt verkocht ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`
+                : `${ticket.data.eventsquareReference ? `Bestelling ${ticket.data.eventsquareReference} is voltooid` : 'Bestelling in uitvoering, neem contact op met de verkoper'}`,
     }
 
     const props = { error, messages };
