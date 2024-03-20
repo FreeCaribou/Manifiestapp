4552df1c5eee64c84a624bc436d1eadfe6f64b04
FreeCaribou <kingdomflo16@gmail.com>
2024-03-20 18:16:56 +0100

    preparation of map and qr code - beta

--- Manifiestapp/src/app/pages/map/map.page.ts
+++ Manifiestapp/src/app/pages/map/map.page.ts
@@ -3,7 +3,6 @@ import { MapCommunicationService } from 'src/app/shared/services/communication/m
 import { Layer, MapOptions } from 'leaflet';
 import { LoadingCommunicationService } from 'src/app/shared/services/communication/loading.communication.service';
 import { IonModal } from '@ionic/angular';
-import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-map',
@@ -30,27 +29,6 @@ export class MapPage {
 
   async scanQrCode() {
     console.log('try to scan')
-
-    document.querySelector('body').classList.add('scanner-active');
-
-    console.log('start scannnn 1')
-    // Check camera permission
-    // This is just a simple example, check out the better checks below
-    await BarcodeScanner.checkPermission({force: true});
-    console.log('start scannnn 2')
-
-    // make background of WebView transparent
-    // note: if you are using ionic this might not be enough, check below
-    BarcodeScanner.hideBackground();
-
-    const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
-    console.log('start scannnn')
-
-    // if the result has content
-    if (result.hasContent) {
-      console.log(result.content); // log the raw scanned content
-    }
-
   }
 
   cancel() {
