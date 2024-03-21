da4679a6652f1f25a95101fffefa6535e716a235
FreeCaribou <kingdomflo16@gmail.com>
2024-03-21 18:56:12 +0100

    fixup! beta qr code

--- Manifiestapp/src/app/pages/map/map.page.ts
+++ Manifiestapp/src/app/pages/map/map.page.ts
@@ -34,50 +34,55 @@ export class MapPage {
     }).add(() => { this.loadingCommunication.changeLoaderTo(false) });
   }
 
+  ionViewWillLeave() {
+    this.stopScan();
+  }
+
   async scanQrCode() {
     console.log('try to scan')
 
-      // The camera is visible behind the WebView, so that you can customize the UI in the WebView.
-  // However, this means that you have to hide all elements that should not be visible.
-  // You can find an example in our demo repository.
-  // In this case we set a class `barcode-scanner-active`, which then contains certain CSS rules for our app.
-  document.querySelector('body')?.classList.add('barcode-scanner-active');
+    // The camera is visible behind the WebView, so that you can customize the UI in the WebView.
+    // However, this means that you have to hide all elements that should not be visible.
+    // You can find an example in our demo repository.
+    // In this case we set a class `barcode-scanner-active`, which then contains certain CSS rules for our app.
+    document.querySelector('body')?.classList.add('barcode-scanner-active');
 
-  // Add the `barcodeScanned` listener
-  const listener = await BarcodeScanner.addListener(
-    'barcodeScanned',
-    async result => {
-      console.log('We have a scan', result.barcode, result.barcode.rawValue, result.barcode.displayValue)
+    // Add the `barcodeScanned` listener
+    const listener = await BarcodeScanner.addListener(
+      'barcodeScanned',
+      async result => {
+        console.log('We have a scan', result.barcode, result.barcode.rawValue, result.barcode.displayValue)
 
-      try {
-        const value = result.barcode.displayValue;
-        if (value.includes('https://manifiesta.be/')) {
-          // TODO be more careful and make verification
-          // Put that in a component just for the qr code, so we put that where ever we want later !
-          // Hide that for web versie
+        try {
+          const value = result.barcode.displayValue;
           const splitted = value.split('/');
-          console.log('we go to ', splitted[splitted.length - 1])
-          this.router.navigate(['programme', 'subprogramme', 'localisation'], { queryParams: { place: splitted[splitted.length - 1] } });
+          if (value.includes('https://manifiesta.be/')) {
+            // TODO be more careful and make verification
+            // Put that in a component just for the qr code, so we put that where ever we want later !
+            // Hide that for web versie
+            
+            console.log('we go to ', splitted[splitted.length - 1])
+            this.router.navigate(['programme', 'subprogramme', 'localisation'], { queryParams: { place: splitted[splitted.length - 1] } });
+          }
+        } catch (e) {
+          this.stopScan();
         }
-      } catch(e) {
 
-      }
+        this.stopScan();
+      },
+    );
 
-      this.stopScan();
-    },
-  );
-
-  // Start the barcode scanner
-  await BarcodeScanner.startScan();
+    // Start the barcode scanner
+    await BarcodeScanner.startScan();
   }
 
   async stopScan() {
     // Make all elements in the WebView visible again
     document.querySelector('body')?.classList.remove('barcode-scanner-active');
-  
+
     // Remove all listeners
     await BarcodeScanner.removeAllListeners();
-  
+
     // Stop the barcode scanner
     await BarcodeScanner.stopScan();
   };
