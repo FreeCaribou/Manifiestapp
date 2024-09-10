d80e68e9e8ded8927f1ed6cb8f8b77a7ae4722ae
Florian Gerard <Florian.Gerard@economie.fgov.be>
2024-09-10 14:34:42 +0200

    better loader

--- manifiestapp-backoffice/src/app/pages/page-home/page-home.component.ts
+++ manifiestapp-backoffice/src/app/pages/page-home/page-home.component.ts
@@ -3,7 +3,9 @@ import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dial
 import { MatPaginator } from '@angular/material/paginator';
 import { Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { tap } from 'rxjs';
 import { SellersService } from 'src/app/shared/services/api/sellers.service';
+import { LoaderService } from 'src/app/shared/services/communication/loader.service';
 import { sortData } from 'src/app/shared/utils/sort-data.utils';
 
 @Component({
@@ -24,49 +26,56 @@ export class PageHomeComponent implements OnInit, AfterViewInit {
   chartOptions = {};
   iLikeChart = false;
 
-  constructor(private sellersService: SellersService, public dialog: MatDialog) { }
+  constructor(
+    private sellersService: SellersService, 
+    public dialog: MatDialog,
+    private loaderService: LoaderService,
+  ) { }
 
   ngAfterViewInit() {
     this.sortedData.paginator = this.paginator;
   }
 
   ngOnInit(): void {
-    this.sellersService.getAllSellerSellingInformation().subscribe(data => {
-      this.sellerSellingInformationsAll = data.data;
-      this.sellingInformationsAmountTickets = data.totalAmountTicket;
-      this.sortedData.data = this.sellerSellingInformationsAll.slice();
-      this.sortedData.data.forEach(sd => {
-        sd.details.forEach((d: any) => {
-          d?.ticketInfo?.forEach((ti: any) => {
-            const index = this.ticketsType.findIndex(tt => tt.id === ti.ticketId);
-            if (index > -1) {
-              this.ticketsType[index].amount += ti.ticketAmount;
-            } else {
-              this.ticketsType.push({ id: ti.ticketId, label: ti.ticketLabel, amount: ti.ticketAmount });
-            }
+    this.loaderService.startLoading(PageHomeComponent.name);
+    this.sellersService.getAllSellerSellingInformation().pipe(
+      tap(data => {
+        this.sellerSellingInformationsAll = data.data;
+        this.sellingInformationsAmountTickets = data.totalAmountTicket;
+        this.sortedData.data = this.sellerSellingInformationsAll.slice();
+        this.sortedData.data.forEach(sd => {
+          sd.details.forEach((d: any) => {
+            d?.ticketInfo?.forEach((ti: any) => {
+              const index = this.ticketsType.findIndex(tt => tt.id === ti.ticketId);
+              if (index > -1) {
+                this.ticketsType[index].amount += ti.ticketAmount;
+              } else {
+                this.ticketsType.push({ id: ti.ticketId, label: ti.ticketLabel, amount: ti.ticketAmount });
+              }
+            });
           });
         });
-      });
-
-      this.chartOptions = {
-        title: {
-          text: "Sales by ticket types"
-        },
-        data: [{
-          type: "pie",
-          startAngle: -90,
-          indexLabel: "{name}: {y} - {q} sellings",
-          yValueFormatString: "#,###.##'%'",
-          dataPoints: this.ticketsType.map(tt => {
-            return {
-              y: tt.amount / this.sellingInformationsAmountTickets * 100,
-              name: tt.label,
-              q: tt.amount
-            }
-          })
-        }]
-      }
-    });
+  
+        this.chartOptions = {
+          title: {
+            text: "Sales by ticket types"
+          },
+          data: [{
+            type: "pie",
+            startAngle: -90,
+            indexLabel: "{name}: {y} - {q} sellings",
+            yValueFormatString: "#,###.##'%'",
+            dataPoints: this.ticketsType.map(tt => {
+              return {
+                y: tt.amount / this.sellingInformationsAmountTickets * 100,
+                name: tt.label,
+                q: tt.amount
+              }
+            })
+          }]
+        }
+      })
+    ).subscribe().add(() => this.loaderService.stopLoading(PageHomeComponent.name));
   }
 
   sortingData(sort: Sort) {
