71df23cd5a930ef1fa5ef6873737662f945f6bbe
FreeCaribou <kingdomflo16@gmail.com>
2024-10-25 09:51:09 +0200

    New recept of data

--- manifiestapp-finish-payment/app/finish-payment/page.tsx
+++ manifiestapp-finish-payment/app/finish-payment/page.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import https from 'https';
 import FinishPaymentComponent from '../shared/finish-payment-component';
-import RedirectToRealFinish from '../shared/redirect-to-real-finish.component';
 
 export default async function FinishPayment({
     searchParams,
@@ -10,16 +9,38 @@ export default async function FinishPayment({
 }) {
     const vivaWalletTransactionId = searchParams?.t;
 
+    const ticketData = axios.get(`${process.env.BASE_URL}tickets/finishOrderPending/${vivaWalletTransactionId}`, {
+        httpsAgent: new https.Agent({
+            rejectUnauthorized: false
+        })
+    }).catch(err => {
+        return err?.response?.data;
+    });
+    const ticket = await Promise.resolve(ticketData);
+    const error: boolean = !vivaWalletTransactionId || !ticket.data?.eventsquareReference;
+
     const messages = {
-        en: '',
-        fr: '',
-        nl: ''
+        en: error ?
+            `There is an error - ${ticket?.message ? ticket?.message[0] : 'unkown'} - ERR CODE ${ticket?.code}`
+            : !ticket?.data?.transactionInProgress ?
+                `Congratulations, the tickets is selling - REF ${ticket?.data?.order?.reference} - You have sold ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`
+                : `${ticket.data.eventsquareReference ? `Order ${ticket.data.eventsquareReference} has been completed` : 'Order in progress, check with the seller'}`,
+        fr: error ?
+            `Une erreur est survenu - ${ticket?.message ? ticket?.message[0] : 'unkown'} - ERR CODE ${ticket?.code}`
+            : !ticket?.data?.transactionInProgress ?
+                `Félicitation, la commande est finalisée - REF ${ticket?.data?.order?.reference} - Tu as vendu ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`
+                : `${ticket.data.eventsquareReference ? `La commande ${ticket.data.eventsquareReference} est terminée` : 'Commande en cours, voir avec le vendeur'}`,
+        nl: error ?
+            `Er is een fout opgetreden - ${ticket?.message ? ticket?.message[0] : 'unkown'} - ERR CODE ${ticket?.code}`
+            : !ticket?.data?.transactionInProgress ?
+                `Gefeliciteerd, uw bestelling is nu voltooid - REF ${ticket?.data?.order?.reference} - Je hebt verkocht ${ticket?.data?.totalTicketsForThisEditionForThisSeller} tickets`
+                : `${ticket.data.eventsquareReference ? `Bestelling ${ticket.data.eventsquareReference} is voltooid` : 'Bestelling in uitvoering, neem contact op met de verkoper'}`,
     }
 
     const props = { messages, vivaWalletTransactionId };
     return (
         <div>
-            <RedirectToRealFinish props={props} />
+            <FinishPaymentComponent props={props} />
         </div>
     )
 }
